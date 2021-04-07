710644ea4d4e07eec7d6574cba2b8ff267f4d2c0
Li Wing Kee <170108350@aluno.unb.br>
2021-04-07 16:19:33 -0300

    Arduino teste

---
+++ spi_teste/ldc1000.h
@@ -0,0 +1,34 @@
+#define SS 10
+#define RD 0x80
+
+#define DVID_REG 0x00
+#define RMAX_REG 0x01
+#define RMIN_REG 0x02
+
+#define WDTF_REG 0x03
+#define LDCC_REG 0x04
+#define CLKC_REG 0x05
+
+#define CTHL_REG 0x06
+#define CTHM_REG 0x07
+#define CTLL_REG 0x08
+#define CTLM_REG 0x09
+
+#define INTB_REG 0x0A
+#define POWR_REG 0x0B
+#define STTS_REG 0x20
+
+#define PRXL_REG 0x21
+#define PRXM_REG 0x22
+
+#define FQCL_REG 0x23
+#define FQCB_REG 0x24
+#define FQCM_REG 0x25
+
+
+void ldcConfig( void );
+void clkConfig( void );
+uint32_t readRegister(uint8_t add, uint8_t nBytes);
+void writeRegister(uint8_t addr, uint8_t data);
+
+
---
+++ spi_teste/spi_teste.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include "LDC1000.h"
+
+
+#define FREQ_PIN 9
+
+
+void setup() {
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+  
+  Serial.begin(9600);
+  SPI.begin();
+
+  clkConfig();
+  ldcConfig();
+  
+}
+
+void loop() {
+  Serial.print("READ REGISTER: 0x");
+  uint32_t out = readRegister(PRXL_REG, 2);
+  Serial.println(out, HEX);
+  
+  delay(5000);
+
+}
+
+
+uint32_t readRegister(uint8_t addr, uint8_t nBytes)
+{
+  uint32_t slaveOut = 0 ;
+  
+  digitalWrite(SS, LOW);
+  
+  SPI.transfer(RD|addr);
+  slaveOut = SPI.transfer(0x00);
+
+  nBytes--;
+  while(nBytes)
+  {
+    slaveOut = ( slaveOut<<8 )  | SPI.transfer(0x00);
+    nBytes--;
+  }
+  
+  digitalWrite(SS, HIGH);
+
+  return slaveOut;
+}
+
+void writeRegister(uint8_t addr, uint8_t data)
+{
+  digitalWrite(SS, LOW);
+  SPI.transfer(addr);
+  SPI.transfer(data);
+  digitalWrite(SS, HIGH);  
+}
+
+void ldcConfig( void ){
+  writeRegister(RMAX_REG, 0x13);
+  writeRegister(RMIN_REG, 0x3D);
+
+  writeRegister(WDTF_REG, 0x93);
+  writeRegister(LDCC_REG, 0x17);
+  writeRegister(CLKC_REG, 0x00);
+
+  writeRegister(CTHL_REG, 0x50);
+  writeRegister(CTHM_REG, 0x14);
+  writeRegister(CTLL_REG, 0xC0);
+  writeRegister(CTLM_REG, 0x12);
+
+  writeRegister(POWR_REG, 0x01);
+}
+
+void clkConfig( void )
+{
+  pinMode(FREQ_PIN, OUTPUT);
+  TCCR1A = ( (1 << COM1A0));
+  TCCR1B = ((1 << WGM12) | (1 << CS10));
+  TIMSK1 = 0;
+  OCR1A = 0;    //0-8MHz\1-4MHZ\3-2MHZ\7-1MHZ
+}
